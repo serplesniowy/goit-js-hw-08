96974adc66995ce2a16a6d62c78116a120287576
serplesniowy <132208333+serplesniowy@users.noreply.github.com>
2025-04-02 18:18:00 +0200

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -9,7 +9,7 @@ const images = [
       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
     original:
       "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
-    description: "Hokkaido Flower"
+    description: "Hokkaido Flower",
   },
   {
     preview:
